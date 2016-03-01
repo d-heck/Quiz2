2166d30fa0778b11b5c4649d0df08020c21fc953
David Heck <dheck@udel.edu>
2016-03-01 00:52:48 -0500

    Final Commit

--- Quiz 2/src/main/java/base/Main.java
+++ Quiz 2/src/main/java/base/Main.java
@@ -1,5 +1,11 @@
 package base;
-
+/*
+ * I believe this code works as it should, I am not 100% on how we were
+ * to calculate the data so I tried my best to reason with it. Hopefully
+ * I have completed the question as it was meant to be.
+ * 
+ *  @David Heck
+ */
 import java.util.Scanner;
 
 public class Main {
@@ -13,46 +19,46 @@ public class Main {
 		// Gather user input. Assuming a 4 year college experience.
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter your inititial tuition cost (per semester): ");
-		double tuition = sc.nextDouble();
+		float tuition = sc.nextFloat();
 		System.out.println("Enter your percentage increases for tuition: ");
-		double percent_inc = sc.nextDouble() / 100;
+		float percent_inc = sc.nextFloat();
 		System.out.println("Enter your APR: ");
-		double APR = sc.nextDouble() / 100;
+		float APR = sc.nextFloat();
 		System.out.println("Enter how long your repayment scheduel will be (in years): ");
-		int Years = sc.nextInt();
+		float Years = sc.nextFloat();
 		sc.close();
 
 		// Methods to gain total tuition cost.
-		double total_tuition = Total_Cost(tuition, percent_inc, APR, Years);
+		float total_tuition = Total_Cost(tuition, percent_inc, APR, Years);
 		System.out.printf("$%,.2f%n", total_tuition);
 		System.out.printf("$%,.2f%n", monthly_payment(total_tuition, Years));
 	}
 
 	// Obtain tuition with percentage increase factored and APR.
-	public static double Total_Cost(double tuition, double percent_inc, double APR, int Years) {
-		double final_tuition = 0;
-		double APR_During = 0;
-		double TOTAL = 0;
+	public static float Total_Cost(float tuition, float percent_inc, float APR, float Years) {
+		float final_tuition = 0;
+		float APR_During = 0;
+		float TOTAL = 0;
 		//I am not sure if year one is supposed to have the percentage increase as well, but I've included it that way.
 		for (int i = 0; i < 4; i++) {
-			final_tuition += ((tuition * 2) + (tuition * percent_inc)); 
-			APR_During += final_tuition * APR; //Calculate the APR value generated during each year @ school.
+			final_tuition += ((tuition * 2) + (tuition * (percent_inc/100))); 
+			APR_During += final_tuition * (APR/100); //Calculate the APR value generated during each year @ school.
 		}
 		
 		TOTAL = APR_During + final_tuition;
 		
 		// Calculate final cost of APR post college (if necessary).
 		if(Years > 4){
-			for (int i = Years-4; i > 0; i--) {
-				TOTAL = TOTAL + (TOTAL * APR);
+			for (float i = Years-4; i > 0; i--) {
+				TOTAL = TOTAL + (TOTAL * (APR/100));
 			}
 		}
 		return TOTAL;
 	}
 
 	// Determine cost per month.
-	public static double monthly_payment(double total, int repayment) {
-		double monthly_payment = total / repayment;
+	public static float monthly_payment(float total, float repayment) {
+		float monthly_payment = total / (repayment*12);
 		return monthly_payment;
 	}
 }
--- Quiz 2/src/test/java/base/Main_Test.java
+++ Quiz 2/src/test/java/base/Main_Test.java
@@ -10,6 +10,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+@SuppressWarnings("unused")
 public class Main_Test {
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -28,7 +29,31 @@ public class Main_Test {
 	}
 	
 	@Test
-	public void test_get_iValue() {
-		assertEquals(value.get_iValue(), 3);
-
+	public void Test_Total_Cost1() {
+		float expectedValue = (float) 111828.64;
+		float actualValue = Main.Total_Cost(12520, 3, 4, 4);
+		System.out.println(actualValue);
+		assertTrue(expectedValue == actualValue);
+	}
+	@Test
+	public void Test_Total_Cost2() {
+		float expectedValue = (float) 130823.695;
+		float actualValue = Main.Total_Cost(12520, 3, 4, 8);
+		System.out.println(actualValue);
+		assertTrue(expectedValue == actualValue);
+	}
+	@Test
+	public void Test_Monthly_Payment1() {
+		float expectedValue = (float) 2329.7634;
+		float actualValue = Main.monthly_payment((float)111828.64,(float) 4);
+		System.out.println(actualValue);
+		assertTrue(expectedValue == actualValue);
+	}
+	@Test
+	public void Test_Monthly_Payment2() {
+		float expectedValue = (float) 1362.747;
+		float actualValue = Main.monthly_payment((float)130823.70, (float)8);
+		System.out.println(actualValue);
+		assertTrue(expectedValue == actualValue);
+	}
 }
